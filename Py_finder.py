ba220c57ec7c56ad013969ff7acb133bf80a874e
AlexFreyjr <alex@Alex-Bureau.localdomain>
2022-04-18 15:27:21 +0200

    better version

--- Py_finder.py
+++ Py_finder.py
@@ -2,13 +2,13 @@ from math import pi
 
 def PyFinder(num): 
     if num >= 15 :
-        print("nope")
+        print("Only numbers between 1 and 15")
     else :
         print(pi)
-
-
-
     
 num = input('Input a number between 1 and 15 : ')
-int_nbr = int(num)
-PyFinder(int_nbr)
+try:
+    int_nbr = int(num)
+    PyFinder(int_nbr)
+except:
+    print("Only numbers please")
