4bc9af9063c24a98759f2c03e0c5e361fe8c78c8
AlexFreyjr <freyjamir@gmail.com>
2022-04-20 09:54:35 +0200

    pseudo code only

---
+++ Youtube downloader.py
@@ -0,0 +1,16 @@
+"""
+pseudo code:
+
+ask for a link to download
+    if connect on youtube
+        ok
+    else
+        raise error and ask again
+ask for where to store the video
+    store the link
+connect to the adress
+    
+launch scrapping
+clean scrap
+send a message to the user with a local link for the video
+"""
