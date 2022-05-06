4650d1f1033fec710ad4b12ac445900b5f7f127c
alexfreyjr <alexfreyjr@gmail.com>
2022-05-06 22:32:56 +0200

    linux commit

--- Py_finder.py
+++ Py_finder.py
@@ -5,7 +5,7 @@ def PyFinder(num):
         print("Only numbers between 1 and 15")
     else :
         print(round(pi,num))
-        
+
 num = input('Input a number between 1 and 15 : ')
 try:
     int_nbr = int(num)
